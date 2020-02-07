b3f814f35b210e52935f520ee54f0ac47ce862a0
Anass_Soliman <anass_soliman@outlook.com>
2020-02-07 15:32:21 -0500

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	ATLAS_URI : 'mongodb+srv://anass:sola2111@cluster0-mmj6p.mongodb.net/test?retryWrites=true&w=majority'
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(express.json());
+
+// DB Config
+const db = require('./config/keys').ATLAS_URI;
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('MongoDB Connected...'))
+	.catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
